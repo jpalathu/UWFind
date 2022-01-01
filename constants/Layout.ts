812cbef8f0455aaa93e2c3066042c390fd43701a
jpalathu <jaypalathunkal@hotmail.com>
2021-12-31 21:54:37 -0500

    add comment

--- constants/Layout.ts
+++ constants/Layout.ts
@@ -1,3 +1,7 @@
+/*
+  This isn't used right now, but might be helpful to have in case 
+  we ever want to do some specific logic using the screen dimensions.
+*/
 import { Dimensions } from 'react-native';
 
 const width = Dimensions.get('window').width;
