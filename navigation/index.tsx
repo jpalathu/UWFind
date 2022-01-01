858d2b2d32af8263940fb1adffa4c18f29cd8ca0
jpalathu <jaypalathunkal@hotmail.com>
2021-12-31 22:20:14 -0500

    remove unused TabBarIcon

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -52,17 +52,6 @@ function RootNavigator() {
   );
 }
 
-/**
- * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
- */
-function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
-  color: string;
-}) {
-  return <FontAwesome size={30} style={{ marginBottom: -3 }} {...props} />;
-}
-
-
 type PressableIconProps = {
   icon: any;
   onPress: () => void;
