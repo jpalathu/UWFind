a6aedc857348ff866e71cce85e0c1bc27440dd9d
jpalathu <jaypalathunkal@gmail.com>
2022-01-08 18:46:48 -0500

    set up user create mutation and query

--- backend/api/schema.py
+++ backend/api/schema.py
@@ -1,39 +1,61 @@
 import graphene
+from graphene.types import mutation
 from graphene_django.types import DjangoObjectType
-from .models import DropOffLocation, Category
+from .models import User, DropOffLocation, Category
 
+####################################################################### User ###############################################################
+class UserType(DjangoObjectType):
+    class Meta:
+        model = User
+
+class UserInput(graphene.InputObjectType):
+    firstName = graphene.String()
+    lastName = graphene.String()
+    email = graphene.String()
+    phoneNumber = graphene.String()
+
+class CreateUser(graphene.Mutation):
+    user = graphene.Field(UserType)
+
+    class Arguments:
+        input = UserInput(required=True)
+
+    def mutate(root, info, input):
+        userInstance = User(
+            firstName=input.firstName,
+            lastName=input.lastName,
+            email=input.email,
+            phoneNumber=input.phoneNumber
+        )
+        userInstance.save()
+        return CreateUser(user=userInstance)
+
+#################################################################### DropOffLocation ###############################################################
 class DropOffLocationType(DjangoObjectType):
     class Meta:
         model = DropOffLocation
 
+##################################################################### Category ###############################################################
 class CategoryType(DjangoObjectType):
     class Meta:
         model = Category
 
-# class CategoryInput(graphene.InputObjectType):
-#     name = graphene.String()
-
-# class CreateCategory(graphene.Mutation):
-#     category = graphene.Field(Category)
-
-#     class Arguments:
-#         input = CategoryInput(required=True)
-    
-#     def mutate(self, info, input):
-#         category = Category(input)
-        # return CreateCategory(category)
-
+#################################################################### Query and Mutation ############################################################
 class Query(graphene.ObjectType):
     drop_off_locations = graphene.List(DropOffLocationType)
     categories = graphene.List(CategoryType)
+    user_by_id = graphene.Field(UserType, userID=graphene.Int())
 
     def resolve_drop_off_locations(root, info):
         return DropOffLocation.objects.all()
 
     def resolve_categories(root, info):
         return Category.objects.all()
+    
+    def resolve_user_by_id(root, info, userID):
+        return User.objects.get(userID=userID)
 
-# class Mutation(graphene.ObjectType):
-#     create_category = CreateCategory.Field()
+class Mutation(graphene.ObjectType):
+    create_user = CreateUser.Field()
 
-schema = graphene.Schema(query=Query)
+schema = graphene.Schema(query=Query, mutation=Mutation)
