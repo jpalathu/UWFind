d792407e199b5d64fd619011bcf6c516cdd62a5d
naz321 <haidernaziba@gmail.com>
2022-02-10 18:44:57 -0500

    added edit and claim item buttons

--- frontend/components/FoundDetailedItem.tsx
+++ frontend/components/FoundDetailedItem.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   Text,
   View,
@@ -6,6 +6,19 @@ import {
   ScrollView,
   Image,
 } from "react-native";
+import {
+  Container,
+  Divider,
+  Modal,
+  FormControl,
+  Input,
+  Button,
+  IconButton,
+  Select,
+} from "native-base";
+import { Foundation } from "@expo/vector-icons";
+import DatePicker from "react-native-datepicker";
+import { gql, useLazyQuery, useMutation } from "@apollo/client";
 
 export default function FoundDetailedItem({ route }) {
   const { itemPostId } = route.params;
@@ -16,9 +29,243 @@ export default function FoundDetailedItem({ route }) {
   const { itemLocation } = route.params;
   const { itemDescription } = route.params;
   const { itemOtherLocation } = route.params;
+  const [showEditInfo, setShowEditInfo] = useState(false);
+  const [date, setDate] = useState(itemDate);
+  const [locationValue, setLocationValue] = useState(itemLocation);
+  const [categoryValue, setCategoryValue] = useState("");
+  const [locations, setLocations] = useState<any[]>([]);
+  const [categories, setCategories] = useState<any[]>([]);
+
+  const [post, setpost] = useState({
+    title: "",
+    date: "",
+    image: "",
+    category: "",
+    location: "",
+    otherLocation: "",
+    description: ""
+  });
+ 
+  const [modalFields, setModalFields] = useState({
+    title: "",
+    date: "",
+    image: "",
+    category: "",
+    location: "",
+    otherLocation: "",
+    description: "",
+  });
+
+  const openModal = () => {
+    setShowEditInfo(true);
+    setModalFields({
+      title: itemTitle,
+      date: itemDate,
+      image: itemImage,
+      category: itemCategory,
+      location: itemLocation,
+      description: itemDescription,
+    });
+  };
+
+  const closeModal = () => {
+    setShowEditInfo(false);
+    setModalFields({
+      title: itemTitle,
+      date: itemDate,
+      image: itemImage,
+      category: itemCategory,
+      location: itemLocation,
+      description: itemDescription,
+    });
+  };
+
+
+  const FORM_DATA = gql`
+  query {
+    categories {
+      name
+      categoryId
+    }
+    buildings {
+      name
+      buildingId
+    }
+  }
+`;
+  const [executeQuery] = useLazyQuery(FORM_DATA);
+  const getFormData = async () => {
+    const { data, error } = await executeQuery();
+    if (error) {
+      console.error("ERROR", JSON.stringify(error, null, 2));
+    } else {
+      setLocations(data.buildings);
+      setCategories(data.categories);
+    }
+  };
+
+  useEffect(() => {
+    getFormData();
+  }, []);
+
+
+
 
   return (
     <View style={styles.container}>
+
+
+
+
+<Modal isOpen={showEditInfo} onClose={closeModal}>
+        <Modal.Content maxWidth="400px">
+          <Modal.CloseButton />
+          <Modal.Header>Edit</Modal.Header>
+          <Modal.Body>
+            <FormControl>
+              <FormControl.Label>Title</FormControl.Label>
+              <Input
+                type="text"
+                onChangeText={(v) =>
+                  setModalFields({ ...modalFields, title: v })
+                }
+                defaultValue={modalFields.title}
+              />
+            </FormControl>
+            <FormControl mt="3">
+              <FormControl.Label>Date</FormControl.Label>
+
+              <DatePicker
+                style={{ width: 200 }}
+                date={date}
+                mode="date"
+                placeholder="select date"
+                format="YYYY-MM-DD"
+                minDate="2022-01-15"
+                maxDate="2025-06-01"
+                confirmBtnText="Confirm"
+                cancelBtnText="Cancel"
+                customStyles={{
+                  dateIcon: {
+                    position: "absolute",
+                    left: 0,
+                    top: 4,
+                    marginLeft: 0,
+                  },
+                  dateInput: {
+                    marginLeft: 36,
+                  },
+                }}
+                onDateChange={(v) => {
+                  setModalFields({ ...modalFields, date: v })
+                  setDate(v);
+                }}
+              />
+
+            </FormControl>
+            <FormControl mt="3">
+              <FormControl.Label>Location</FormControl.Label>
+              <Select 
+             selectedValue={locationValue}
+             minWidth={200}
+             accessibilityLabel="Select a Location"
+             placeholder="Select a Location"
+             onValueChange={(v) => {
+               setModalFields({ ...modalFields, location: v })
+               setLocationValue(v);
+             }}
+             _selectedItem={{ bg: "yellow.400" }}
+             mt={1}
+           >
+              {locations.map((location) => (
+                        <Select.Item
+                          key={location.buildingId}
+                          label={location.name}
+                          value={location.buildingId}
+                        />
+                      ))}
+              </Select>
+              {/* <Input
+                type="text"
+                onChangeText={(v) => setModalFields({ ...modalFields, location: v })}
+                defaultValue={modalFields.location}
+              /> */}
+            </FormControl>
+              {/* INSERT LIST OF DROP OFF LOCATIONS HERE */}
+            <FormControl mt="3">
+              <FormControl.Label>Drop-off Location</FormControl.Label>
+              <Input
+                type="text"
+                onChangeText={(v) =>
+                  setModalFields({ ...modalFields, otherLocation: v })
+                }
+                defaultValue={modalFields.otherLocation}
+              />
+            </FormControl>
+            <FormControl mt="3">
+              <FormControl.Label>Category</FormControl.Label>
+
+              <Select
+                selectedValue={categoryValue}
+                minWidth={200}
+                accessibilityLabel="Select a Category"
+                placeholder="Select a Category"
+                onValueChange={(itemValue) => {
+                  setModalFields({ ...modalFields, location: itemValue })
+                  setCategoryValue(itemValue);
+                }}
+                _selectedItem={{ bg: "yellow.400" }}
+                mt={1}
+              >
+                {categories.map((category) => (
+                  <Select.Item
+                    key={category.categoryId}
+                    label={category.name}
+                    value={category.categoryId}
+                  />
+                ))}
+              </Select>
+              {/* <Input
+                type="text"
+                onChangeText={(v) => setModalFields({ ...modalFields, category: v })}
+                defaultValue={modalFields.category}
+              /> */}
+            </FormControl>
+            <FormControl mt="3">
+              <FormControl.Label>Description</FormControl.Label>
+              <Input
+                type="text"
+                onChangeText={(v) => setModalFields({ ...modalFields, description: v })}
+                defaultValue={modalFields.description}
+              />
+            </FormControl>
+            <FormControl mt="3">
+              <FormControl.Label>Image</FormControl.Label>
+              <Input
+                type="text"
+                onChangeText={(v) => setModalFields({ ...modalFields, image: v })}
+                defaultValue={modalFields.image}
+              />
+            </FormControl>
+    
+           
+          </Modal.Body>
+          <Modal.Footer>
+            <Button.Group space={2}>
+              <Button
+                variant="ghost"
+                colorScheme="blueGray"
+                onPress={closeModal}
+              >
+                Cancel
+              </Button>
+              {/* <Button isLoading={isMutationLoading} onPress={updateProfile}>
+                Update
+              </Button> */}
+            </Button.Group>
+          </Modal.Footer>
+        </Modal.Content>
+      </Modal>
       <View style={styles.header}>
         <View style={styles.header_text}>
           
@@ -30,6 +277,18 @@ export default function FoundDetailedItem({ route }) {
       <ScrollView style={styles.news_container}>
       <View style={styles.news_item}>
               <View style={styles.text_container}>
+              <Container style={{ alignSelf: "flex-end" }}>
+            <Container style={{ alignSelf: "stretch" }}>
+              <IconButton
+                variant="solid"
+                _icon={{
+                  as: Foundation,
+                  name: "pencil",
+                }}
+                onPress={openModal}
+              />
+            </Container>
+          </Container>
               {/* <Text style={styles.title}>{itemPostId}</Text> */}
 
                 <Text style={styles.title}>{itemTitle}</Text>
@@ -50,7 +309,25 @@ export default function FoundDetailedItem({ route }) {
                 <Image source={{ uri: itemImage }} style={styles.photo} />
               </View>
 
-              <Text style={styles.news_text}>*PLACEHOLDER FOR CLAIM ITEM*</Text>
+              <Button
+                  
+                  size="lg"
+                  my="6"
+                  style={{
+                    backgroundColor: "#ffc50b",
+                    borderColor: "#000",
+                    borderWidth: 1,
+                    shadowOpacity: 0.3,
+                    shadowRadius: 10,
+                    shadowOffset: { width: 1, height: 10 },
+                  }}
+                  width="40%"
+                  height="39px"
+                  borderRadius="20"
+                  _text={{ color: "#000" }}
+                >
+                Claim Item
+                </Button>
 
 
               </View>
