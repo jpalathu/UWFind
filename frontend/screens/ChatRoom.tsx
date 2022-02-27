1d5ae512a7b5ec1059d824bfcaaf9d6eb42d65a1
jpalathu <jaypalathunkal@gmail.com>
2022-02-26 20:29:20 -0500

    fix title of chat room

--- frontend/screens/ChatRoom.tsx
+++ frontend/screens/ChatRoom.tsx
@@ -2,7 +2,7 @@ import { useNavigation, useRoute } from "@react-navigation/native";
 import React, { useState } from "react";
 import { useEffect } from "react";
 import { ActivityIndicator } from "react-native";
-
+import { useStore } from "../store";
 import {
   Channel,
   MessageList,
@@ -12,6 +12,7 @@ import {
 } from "stream-chat-expo";
 
 const ChatRoom = () => {
+  const { userID } = useStore();
   const route = useRoute();
   const navigation = useNavigation();
   const { client } = useChatContext();
@@ -27,10 +28,17 @@ const ChatRoom = () => {
       const channels = await client.queryChannels({ id: { $eq: channelID } });
       if (channels.length > 0) {
         setChannel(channels[0]);
+        let members = await channels[0].queryMembers({});
+        let otherMember = members.members.filter(
+          (member) => member.user_id != userID
+        );
+
+        navigation.setOptions({
+          title: otherMember.length > 0 ? otherMember[0].user.name : "",
+        });
       } else {
         console.log("No channels found");
       }
-      navigation.setOptions({ title: "PLACEHOLDER" });
     };
 
     getChannel();
