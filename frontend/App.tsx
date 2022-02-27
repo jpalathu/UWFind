87b566b9c613acb3b2787ee8495f612bd6188218
jpalathu <jaypalathunkal@gmail.com>
2022-02-27 00:42:58 -0500

    ignore annoying logs

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -10,6 +10,7 @@ import * as ImagePicker from "expo-image-picker";
 import Constants from "expo-constants";
 import { StreamChat } from "stream-chat";
 import { OverlayProvider, Chat } from "stream-chat-expo";
+import IGNORE_LOGS from "./ignoreLogs";
 
 const chatClient = StreamChat.getInstance("yjnpxas6ctem");
 
@@ -30,12 +31,7 @@ const client = new ApolloClient({
 });
 
 // Ignoring these annoying logs
-LogBox.ignoreLogs([
-  "NativeBase:",
-  "VirtualizedList:",
-  "When server rendering,",
-  "Each child in a list"
-]);
+LogBox.ignoreLogs(IGNORE_LOGS);
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
---
+++ frontend/ignoreLogs.js
@@ -0,0 +1,11 @@
+// Ignoring these annoying logs
+export default [
+  "NativeBase:",
+  "VirtualizedList:",
+  "When server rendering,",
+  "Each child in a list",
+  "Animated: `useNativeDriver` was not specified.",
+  "Please update the following components:",
+  "Warning: componentWillReceiveProps has been renamed",
+  "DatePickerIOS has been merged with DatePickerAndroid and will be removed in a future release.",
+];
