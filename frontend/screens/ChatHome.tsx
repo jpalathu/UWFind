c865f8b8601a96575c5024f9b440b1da6d49914e
jpalathu <jaypalathunkal@gmail.com>
2022-03-01 23:36:26 -0500

    fix chat

--- frontend/screens/ChatHome.tsx
+++ frontend/screens/ChatHome.tsx
@@ -2,17 +2,21 @@ import React from "react";
 import { View, StyleSheet } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { ChannelList } from "stream-chat-expo";
+import { useStore } from "../store";
 
 const ChatHome = () => {
   const navigation = useNavigation();
+  const { userID } = useStore();
 
-  const goToChatRoom = (channel) => {
+  const goToChatRoom = (channel: any) => {
     navigation.navigate("ChatRoom", { channelID: channel.id });
   };
 
+  const filters = { members: { $in: [`${userID}`] } };
+
   return (
     <View style={styles.container}>
-      <ChannelList onSelect={goToChatRoom} />
+      <ChannelList onSelect={goToChatRoom} filters={filters} />
     </View>
   );
 };
